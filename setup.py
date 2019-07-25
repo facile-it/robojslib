970533ff97be1f9139b2f1cc3a5c155db1bb66a1
Andrea Gubellini <andrea.gubellini@facile.it>
2019-07-25 14:49:07 +0200

    updating to 0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="robojslib",
-    version="0.3.0",
+    version="0.3.1",
     author="Andrea Gubellini",
     author_email="agubellini@yahoo.com",
     description="A simple RobotFramework keyword library written using Vanilla/Python",
